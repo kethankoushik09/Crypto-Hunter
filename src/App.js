fa9b158d657134e2c86f6da0bfd923bcce9dba0a
kethan <kethankoushik09@gmail.com>
2024-12-28 12:33:12 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import React from 'react'
 import "./index.css"
 import Homepage from './pages/Homepage'
 import CustomCoincontext from './context/Coincontext'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { createHashRouter,RouterProvider } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Text from './components/Text/Text'
 import Coin from './components/Coin/Coin'
@@ -14,7 +14,7 @@ import Coin from './components/Coin/Coin'
 
 
 const App = () => {
-  const rt = createBrowserRouter([
+  const rt = createHashRouter([
     {path:"/",element:<Navbar/>,
       children:[
       {index:true,element:<Text/>},
