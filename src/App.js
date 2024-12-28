e6e523572a69139b02ef7c09abb79166a4f19fcc
kethan <kethankoushik09@gmail.com>
2024-12-28 12:55:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
 
+// import React from 'react'
+// import "./index.css"
+// import Homepage from './pages/Homepage'
+// import CustomCoincontext from './context/Coincontext'
+// import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+// import Navbar from './components/navbar/Navbar'
+// import Text from './components/Text/Text'
+// import Coin from './components/Coin/Coin'
 
 
 
-import React from 'react'
-import "./index.css"
-import Homepage from './pages/Homepage'
-import CustomCoincontext from './context/Coincontext'
-import { createHashRouter,RouterProvider } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
-import Text from './components/Text/Text'
-import Coin from './components/Coin/Coin'
+// const App = () => {
+//   const rt = createBrowserRouter([
+//     {path:"/",element:<Navbar/>,
+//       children:[
+//       {index:true,element:<Text/>},
+//       {path:"/coin/:id",element:<Coin/>}
 
+//     ]}]
+//   )
+//   return (
+//     <CustomCoincontext>
+//       <div className='app'>
+//         <RouterProvider router={rt}></RouterProvider>
+//       </div>
+//     </CustomCoincontext>
+//   )
+// }
 
+// export default App
+
+// because git not supporting above createBrowser 
+import React from 'react';
+import "./index.css";
+import Homepage from './pages/Homepage';
+import CustomCoincontext from './context/Coincontext';
+import { HashRouter, Route, Routes } from 'react-router-dom';  // Changed to HashRouter
+import Navbar from './components/navbar/Navbar';
+import Text from './components/Text/Text';
+import Coin from './components/Coin/Coin';
 
 const App = () => {
-  const rt = createHashRouter([
-    {path:"/",element:<Navbar/>,
-      children:[
-      {index:true,element:<Text/>},
-      {path:"/coin/:id",element:<Coin/>}
-
-    ]}]
-  )
   return (
     <CustomCoincontext>
-      <div className='app'>
-        <RouterProvider router={rt}></RouterProvider>
-      </div>
+      <HashRouter> {/* Use HashRouter */}
+        <div className='app'>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Text />} />
+            <Route path="/coin/:id" element={<Coin />} />
+          </Routes>
+        </div>
+      </HashRouter>
     </CustomCoincontext>
-  )
+  );
 }
 
-export default App
-
+export default App;
